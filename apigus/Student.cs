33bc6ad37ef12c426501db2bec8d46c6153d427c
nuragus <nuragus.linux@gmail.com>
2016-02-03 14:44:16 +0700

    Commit Restful JSON using WCF

---
+++ apigus/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace apigus
+{
+    [DataContract]
+    public class Student
+    {
+        [DataMember]
+        public int NoMahasiswa {get; set;}
+        [DataMember]
+        public string Nama { get; set; }
+        [DataMember]
+        public string Alamat { get; set; }
+        [DataMember]
+        public int Umur { get; set; }
+    }
+
+    public partial class Students
+    {
+        private static readonly Students _instance = new Students();
+
+        private Students() {}
+
+        public static Students Instance
+        {
+            get { return _instance; }
+        }
+
+        public List<Student> StudentList
+        {
+            get { return students; }
+        }
+
+        private List<Student> students = new List<Student>()
+        {
+            new Student() {NoMahasiswa=1, Nama="agus",Alamat="Bogor", Umur=18}
+        };
+    }
+}
